15bcf041752851f43e9945212bab1269054eae67
AHOhhhh <906626481@qq.com>
2018-09-17 21:57:03 +0800

    [WJ]feat: add container padding

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const maxWidth = 750;
 const gridNumber = 10;
+const containerPadding = 30;
 let documentElement = document.documentElement;
 document.addEventListener('DOMContentLoaded', updateFontSize);
 window.addEventListener('orientationchange', updateFontSize);
@@ -8,5 +9,5 @@ window.addEventListener('resize', updateFontSize);
 function updateFontSize() {
     let width = documentElement.clientWidth;
     width = width < maxWidth ? width : maxWidth;
-    documentElement.style.fontSize = `${width / gridNumber}px`;
+    documentElement.style.fontSize = `${(width - containerPadding) / gridNumber}px`;
 }
